6689da5acbec73d043b2d6b77df3279166289a0c
coffeeaddictamoeba <vika.pokalo.2023@gmail.com>
2025-11-02 16:16:55 +0200

    implemented messaging page UI

--- moodle_site/moodle_site/urls.py
+++ moodle_site/moodle_site/urls.py
@@ -1,11 +1,12 @@
 """moodle_site URL Configuration."""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from home.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
+    path("messages/", include("messaging.urls")),
 ]
