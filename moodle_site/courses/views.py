d40735ed91c65533cc76e44e27d54b9373d8018c
shetakubas <jokubas.sataitis@gmail.com>
2025-11-22 01:42:44 +0200

    Added courses

---
+++ moodle_site/courses/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Course, CourseMaterial
+
+
+def courses_list(request: HttpRequest) -> HttpResponse:
+    """Display all available courses."""
+    courses = Course.objects.filter(is_active=True)
+    return render(request, "courses/courses_list.html", {"courses": courses})
+
+
+def course_detail(request: HttpRequest, course_id: int) -> HttpResponse:
+    """Display a specific course and its materials."""
+    course = get_object_or_404(Course, id=course_id, is_active=True)
+    materials = course.materials.all()
+    return render(request, "courses/course_detail.html", {
+        "course": course,
+        "materials": materials
+    })
+
+
+def material_detail(request: HttpRequest, material_id: int) -> HttpResponse:
+    """Display a specific course material."""
+    material = get_object_or_404(CourseMaterial, id=material_id)
+    return render(request, "courses/material_detail.html", {"material": material})
